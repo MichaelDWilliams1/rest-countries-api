263715539a066114c0f7b1a8cadb94b4c579b865
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2022-08-17 13:14:38 -0700

    removed useless css code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
  
   return (
   <>
-    { loading ?  <section className={`${darkMode && 'dark'} h-screen w-screen`}><Spinner /> </section> :
+    { loading ?  <section className={`${darkMode && 'dark'} h-screen w-screen`}> <Spinner /> </section> :
     <section className={`${darkMode && 'dark'} h-screen w-screen`}>
       <Nav />
       <Routes>
